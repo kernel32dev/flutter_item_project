14fec144a37c61890f0c2f98d2b5d7082ff2a930
kernel32dev <kernel32dev@gmail.com>
2024-12-09 15:34:42 -0300

    upload

---
+++ lib/Model/ItemModel.dart
@@ -0,0 +1,5 @@
+class ItemModel {
+  String nome;
+  bool check;
+  ItemModel({required this.nome, required this.check});
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_item_project/Views/MyApp.dart';
+
+void main() {
+  runApp(const MyApp());
+}
